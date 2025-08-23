432a3f184d12414ce89cde9aa6ffb304309ad460
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-08-24 01:15:17 +0300

    feat: добавлен интерфейс IWriter для записи данных из ByteArray
    Создан абстрактный интерфейс IWriter, определяющий метод записи данных:
    
    - write(const ByteArray& byte) — запись содержимого ByteArray.
    
    Интерфейс используется как базовый контракт для разных реализаций
    (например, запись в файл, сетевой сокет или буфер памяти).

---
+++ include/cxxweb/data/iwriter.h
@@ -0,0 +1,13 @@
+#ifndef IWRITER_H
+#define IWRITER_H
+#include"bytearray.h"
+namespace CxxWeb
+{
+    class IWriter
+    {
+    public:
+        virtual ~IWriter() = default;
+        virtual size_t  write(ByteArray byte);
+    };
+}
+#endif
