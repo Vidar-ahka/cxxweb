31bc54c538d8bc8ddea1b88cff7afdcdf5cbb6e6
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-08-24 01:24:23 +0300

    feat: добавлен интерфейс IConnection для работы с сетевыми соединениями
    Создан интерфейс IConnection, объединяющий IReader и IWriter.
    Определены базовые методы:
    
    - getSocket() — возвращает файловый дескриптор сокета;
    - is_valid() — проверяет корректность соединения.
    
    Интерфейс служит контрактом для реализации конкретных соединений
    (например, TCP/SSL).

--- include/cxxweb/net/iconnection.h
+++ include/cxxweb/net/iconnection.h
@@ -2,21 +2,19 @@
 #define ICONNECTION_H
 
 #include"stdint.h"
-#include"cxxweb/byte/bytearray.h"
+#include"cxxweb/data/bytearray.h"
+#include"cxxweb/data/ireader.h"
+#include"cxxweb/data/iwriter.h"
+
 namespace CxxWeb
 {
-    class IConnection
+    class IConnection : public  IWriter , public IReader
     {
     public:
-        virtual  size_t  send(const ByteArray & byte)   = 0;
-        virtual  ByteArray read(size_t read_size)   = 0;
-        
-        virtual  ByteArray readall()   = 0;
-        virtual  int  getSocket()   = 0;
-        virtual  bool is_valid()  = 0;
-        
         virtual ~IConnection() = default;
-    protected:
+       
+        virtual  int  getSocket() const    = 0;
+        virtual  bool is_valid()  const  = 0;
     };
 }
-#endif
+#endif 
