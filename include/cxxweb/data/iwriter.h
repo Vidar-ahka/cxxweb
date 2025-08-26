8ea1d00fdcae364f1f52831455f6f8175b522114
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-08-26 21:32:00 +0300

    fix: исправлены объявления методов в IWriter
    - методы write(ByteArray) и write(const std::string&) сделаны чисто виртуальными (=0)
    - устранена ошибка с отсутствием `=0`, из-за которой класс нельзя было использовать как интерфейс

--- include/cxxweb/data/iwriter.h
+++ include/cxxweb/data/iwriter.h
@@ -1,14 +1,15 @@
 #ifndef IWRITER_H
 #define IWRITER_H
 #include"bytearray.h"
+#include"string"
 namespace CxxWeb
 {
     class IWriter
     {
     public:
         virtual ~IWriter() = default;
-        virtual size_t  write(ByteArray byte);
-        virtual size_t  write(const std::string & str);       
+        virtual size_t  write(ByteArray byte) = 0 ;
+        virtual size_t  write(const std::string & str) = 0;       
     };
 }
 #endif
