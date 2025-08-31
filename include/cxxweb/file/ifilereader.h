8c73997d83fb1062c75d66bdcfc8948d0b72b4be
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-08-31 15:43:26 +0300

    feat: добавлен интерфейс IFileReader
    - Интерфейс наследует IReader
    - Определены методы для работы с файлами:
      - open(), close()
      - is_open(), empty(), size()
      - path() для получения пути к файлу
    - Заложена основа для абстрактной реализации классов чтения файлов

---
+++ include/cxxweb/file/ifilereader.h
@@ -0,0 +1,23 @@
+#ifndef IFILEREADER_H
+#define IFILEREADER_H
+#include"cxxweb/data/ireader.h"
+namespace CxxWeb
+{
+    class IFileReader : public IReader
+    {
+    public:
+        virtual ~IFileReader() = default;
+        virtual bool open()    = 0; 
+        virtual bool open(const std::string  & str)    = 0;
+        
+        virtual bool close()   = 0;
+        
+        virtual bool is_open() const  = 0;
+        virtual bool empty() const = 0;
+        virtual size_t size() const = 0;                 
+        virtual std::string  path() const  = 0;
+    };
+
+}
+
+#endif
