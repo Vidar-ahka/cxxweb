310029f1dcbfe92fd9683a06c0f587cb2d85f5e6
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-09-02 20:17:22 +0300

    feat(IFileReader): добавлены новые методы для управления чтением
    - endread() — проверяет, достигнут ли конец чтения файла.
    - reload()  — сбрасывает состояние чтения, позволяя начать чтение заново.

--- include/cxxweb/file/ifilereader.h
+++ include/cxxweb/file/ifilereader.h
@@ -9,13 +9,19 @@ namespace CxxWeb
         virtual ~IFileReader() = default;
         virtual bool open()    = 0; 
         virtual bool open(const std::string  & str)    = 0;
+        virtual void  reload()    = 0 ;
         
+
         virtual bool close()   = 0;
         
         virtual bool is_open() const  = 0;
-        virtual bool empty() const = 0;
+        virtual bool empty()   const = 0;
+        virtual bool endread()    = 0 ;
+        
+      
         virtual size_t size() const = 0;                 
         virtual std::string  path() const  = 0;
+
     };
 
 }
