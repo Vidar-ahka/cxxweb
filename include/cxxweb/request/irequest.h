7ce239201dbcf9d952dec65dde5feeb1522f1e75
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-11-08 22:36:26 +0300

    feat(request): добавлен интерфейс IRequest для абстракции HTTP-запросов
    - Добавлен новый интерфейс IRequest, описывающий базовые операции с HTTP-запросами.
    - Интерфейс включает методы:
      - getMethod() — возвращает HTTP-метод (GET, POST и т.д.)
      - getPath() — возвращает путь запроса
      - getHeader(key) — возвращает значение заголовка по ключу
      - hasHeader(key) — проверяет наличие заголовка
    - Подготовка к созданию универсальных обработчиков запросов.

---
+++ include/cxxweb/request/irequest.h
@@ -0,0 +1,19 @@
+#ifndef IHTTPREQUEST_H
+#define IHTTPREQUEST_H
+#include"string"
+
+namespace CxxWeb
+{
+class IRequest {
+public:
+    virtual ~IRequest() = default;
+    virtual std::string getMethod() const = 0;
+    virtual std::string getPath() const = 0;
+    virtual std::string getHeader(std::string key) = 0;
+    virtual bool hasHeader(      std::string key) const = 0;
+};
+
+}
+
+
+#endif
