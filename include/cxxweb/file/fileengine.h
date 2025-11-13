5ad258ef065ec67f370023c7794143f2c895323b
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-11-14 02:17:24 +0300

    feat(FileEngine): добавлен класс для работы с шаблонами и статическими файлами
    - Реализован класс FileEngine для управления путями к шаблонам и статическим файлам.
    - Добавлены методы установки путей: setPathTemplate() и setPathStatic().
    - Методы получения содержимого файлов:
      - getTemplate(path) — возвращает содержимое шаблона.
      - getTemplate(path, keys) — возвращает шаблон с подставленными значениями ключей.
      - getStatic(path) — возвращает содержимое статического файла.
    - Внутреннее кеширование файлов реализовано через unordered_map для ускорения доступа.
    - Приватный метод readFile() обеспечивает чтение файла в ByteArray.

---
+++ include/cxxweb/file/fileengine.h
@@ -0,0 +1,41 @@
+#ifndef FILEENGINE_H
+#define FILEENGINE_H
+
+#include"unordered_map"
+#include"memory"
+#include"cxxweb/data/bytearray.h"
+#include"cxxweb/template/htmltemplatekey.h"
+#include"cxxweb/file/staticfilereader.h"
+
+namespace CxxWeb
+{
+
+
+class FileEngine
+{
+public:
+    FileEngine()=default;
+
+    FileEngine( const std::string & path_template ,const  std::string & path_static);
+
+    ~FileEngine() = default;        
+    void setPathTemplate(const std::string & path_template);
+    void setPathStatic(const std::string   & path_static  );
+
+    
+    ByteArray getTemplate(const std::string & path);
+    ByteArray getTemlate(const std::string & path,std::unordered_map<std::string , std::string> & keys);
+    ByteArray getStatic(const std::string & path);
+    
+private:
+    ByteArray readFile(std::string path);
+
+
+    std::string path_static;
+    std::string path_template;
+    std::unordered_map<std::string , ByteArray > static_map;
+    std::unordered_map<std::string , ByteArray > template_map;
+    std::unordered_map<std::string , std::shared_ptr<HTMLTemplateKey>> template_key_map;    
+};
+}
+#endif
