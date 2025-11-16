dfcfc994af9838105871e7804522009eb46f2418
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-11-16 23:42:15 +0300

    feat(IServer): добавлен метод getSocket , перегрузка start и isValid
    - В интерфейс IServer добавлен чисто виртуальный метод getSocket() для получения дескриптора сокета сервера.
    - Добавлен перегруженный метод start() без параметров для запуска сервера без указания порта.
    - Существующий метод start(uint16_t port) и stop() остаются виртуальными.
    - Улучшает гибкость интерфейса для управления сервером и доступ к его сокету.

--- include/cxxweb/net/iserver.h
+++ include/cxxweb/net/iserver.h
@@ -4,9 +4,12 @@
 class IServer
 {
 public:
-    virtual bool start(uint16_t port) = 0;
-    virtual bool stop() = 0;
     virtual ~IServer()= default;
+    virtual bool start() = 0;
+    virtual bool start(uint16_t port) = 0;
+    virtual bool stop() = 0;    
+    virtual int  getSocket() const = 0;
+    virtual bool isValid() const =0;
 };
 
 #endif
